5408630b34f8014d9a5f64788df2866b9981a354
Michael Landinger <landimichi1@gmail.com>
2025-10-25 15:51:43 +0200

    bugfix: fix FUOC and changed font color in darkmode

---
+++ js/theme-init.js
@@ -0,0 +1,10 @@
+(function() {
+    const savedTheme = localStorage.getItem('theme');
+    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+    } else if (systemPrefersDark) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+})();
